82a520ddfa57e1f6ffa542af434a0a59cbc9cedb
Your Name <you@example.com>
2025-11-25 01:16:43 +0800

    fix ble, bne

--- Ble.java
+++ Ble.java
@@ -5,7 +5,7 @@ public class Ble extends Instructions {
 
     @Override
     public void assemble() {
-        formatCode = String.format("0%s%s%sXX%s", fitWidth(asmParts[1], 3), fitWidth(asmParts[2], 3), fitWidth(asmParts[3], 3), "0001");
+        formatCode = String.format("0%s%s%sXX%s", fitWidth(asmParts[2], 3), fitWidth(asmParts[1], 3), fitWidth(asmParts[3], 3), "0001");
         binaryCode = formatCodeToBinaryCode(formatCode);
     }
 }
